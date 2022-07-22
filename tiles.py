df140d92171047ec909eff18e248ffe0da1091b0
Eden Xiang <xiang_xuan@qq.com>
2022-07-23 01:55:07 +0800

    :sparkles: feat: basic tiles information (suit-id dict)

---
+++ tiles.py
@@ -0,0 +1,5 @@
+tile_index = {'characters': range(0, 9),
+              'dots': range(9, 18),
+              'sticks': range(18, 27),
+              'winds': range(27, 31),
+              'dragons': range(31, 34),}
