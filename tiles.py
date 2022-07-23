6c1f64401d67f3b8c462cd10556193e74cc80230
Eden <xiang_xuan@qq.com>
2022-07-23 13:09:45 +0800

    :sparkles: feat: Tile object draft

--- tiles.py
+++ tiles.py
@@ -3,3 +3,16 @@ tile_index = {'characters': range(0, 9),
               'sticks': range(18, 27),
               'winds': range(27, 31),
               'dragons': range(31, 34),}
+
+
+class Tile:
+    def __init__(self):
+        self.id = None
+        self.owner = None
+        self.isDora = False
+        self.isRed = False
+        self.isGreen = False
+        self.isMiddle = False
+
+    def __str__(self):
+        return self.id
