06880d0b73767cf6e92029877276ae0e42ae94a7
Eden Xiang <xiang_xuan@qq.com>
2022-07-26 01:26:10 +0800

    :recycle: refactor: Package player info code into Player class

--- game.py
+++ game.py
@@ -1,72 +1,71 @@
+from icecream import ic
 import random
 from collections import deque
 from dora_indicator import Indicator
 from tiles import tile_index
 from emoji import id_to_emoji
+from player import Player
 
 # game init
 main_wind = 0  # east wind
-player_1_wind = 0  # east
-player_2_wind = 1  # south
-player_3_wind = 2  # west
-player_4_wind = 3  # north
+player_wind = [0, 1, 2, 3]
+random.shuffle(player_wind)
+player_1 = Player()
+player_2 = Player()
+player_3 = Player()
+player_4 = Player()
+player_list = [player_1, player_2, player_3, player_4]
+for p, w in zip(player_list, player_wind):
+    p.wind = w
 
-player_1_tile_river = []
-player_2_tile_river = []
-player_3_tile_river = []
-player_4_tile_river = []
+ic(player_1.wind)
 
 # shuffle cards
 stack = list(range(0, 34)) * 4
 stack = deque(stack)
 random.shuffle(stack)
-print(stack)
 
+# ace tile
 ace = []
 for i in range(14):
     ace.append(stack.pop())
 ace = ace[::-1]
 print(f"ace: {[id_to_emoji(i) for i in ace]}")
 
+
+# dora
 dora_indicator = Indicator()
 
-# deal
-player_1 = []
-player_2 = []
-player_3 = []
-player_4 = []
+# deal tile
 for i in range(3):
-    for i in range(4): player_1.append(stack.popleft())
-    for i in range(4): player_2.append(stack.popleft())
-    for i in range(4): player_3.append(stack.popleft())
-    for i in range(4): player_4.append(stack.popleft())
+    for p in player_list:
+        for i in range(4):
+            p.hand.append(stack.popleft())
 
-player_1.append(stack.popleft())
-player_2.append(stack.popleft())
-player_3.append(stack.popleft())
-player_4.append(stack.popleft())
+for p in player_list:
+    p.hand.append(stack.popleft())
 
-player_1.sort()
-print(player_1)
-for i in sorted(player_1):
+player_1.hand.sort()
+for i in sorted(player_1.hand):
     print(id_to_emoji(i), end='')
 
 # draw
-order = [player_1, player_2, player_3, player_4]
-while True:
-    if not stack:
-        break
-    for player in order:
-        if not stack:
-            break
-        player.append(stack.popleft())
-        #  self-drawn
-        # discarding a tile
-        # chow pung kong
-        # skip player
-    else:
-        continue
-    break
+# order = [player_1, player_2, player_3, player_4]
+# while True:
+#     if not stack:
+#         break
+#     for player in order:
+#         if not stack:
+#             break
+#         #  self-drawn
+#         player.append(stack.popleft())
+#         # discarding a tile
+#
+#         # chow pung kong
+#         # skip player
+#     else:
+#         continue
+#     break
 
 
 # count pts
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player(object):
+    def __init__(self):
+        self.tile_river = []
+        self.hand = []
+        self.wind = -1
