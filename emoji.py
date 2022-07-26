53e2d7ebee232ff565b69e4c2a45d9a54f2afc5a
Eden Xiang <xiang_xuan@qq.com>
2022-07-26 19:17:19 +0800

    :sparkles: feat: add list_to_emoji func

--- emoji.py
+++ emoji.py
@@ -6,11 +6,20 @@ offset = {'characters': 126983,
           'winds': 126976-27,
           'dragons': 126980-31}
 
+
 def id_to_emoji(id):
     return chr(id + offset[id2suit(id)])
+
+
+def list_to_emoji(list):
+    _s = ''
+    for i in list:
+        _s += id_to_emoji(i)
+    return _s
 
 
 if __name__ == '__main__':
     print(id2suit(1))
     print(id_to_emoji(9))
+    print(list_to_emoji([1, 2, 3, 4]))
 
