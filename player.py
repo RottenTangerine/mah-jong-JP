28fffbf905b410d027a518c6079a079009b9e705
Eden Xiang <xiang_xuan@qq.com>
2022-07-26 19:16:24 +0800

    :sparkles: feat: more information is hold in Player class
    - point
    - str optimize

--- player.py
+++ player.py
@@ -1,5 +1,15 @@
+from emoji import id_to_emoji
+from tiles import id2wind
+
 class Player(object):
-    def __init__(self):
+    wind_list = ['east', 'south', 'west', 'north']
+
+    def __init__(self, name='Default'):
+        self.name = name
+        self.point = 25000
         self.tile_river = []
         self.hand = []
         self.wind = -1
+
+    def __str__(self):
+        return f"{self.name}({self.point})  Wind:{self.wind_list[self.wind]}"
